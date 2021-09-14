cd105bad3c649dbdc238c18f72da1b719ed9024e
Gal <galpalas89@gmail.com>
2021-09-14 12:21:23 +0300

    connect apollo client to graphql server

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import ProductList from "components/productList";
 import "./App.css";
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+});
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="display-1">G.P Electric</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1 className="display-1">G.P Electric</h1>
+        <ProductList />
+      </div>
+    </ApolloProvider>
   );
 }
 
